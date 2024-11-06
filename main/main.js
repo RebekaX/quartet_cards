97660c7f7f9339b721e3be21da2a912c12e46136
Rebeka Tot <bekatot@gmail.com>
2024-11-06 22:54:07 +0100

    categories based on data.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
-    // Assuming 'data' is an array of animal objects available globally
     $.each(data, function(index, animal) {
-        // Clone the existing card structure
         let cardTemplate = $('.animalCardWrapper').first().clone();
         
-        // Update the cloned card with the animal's data
         cardTemplate.find('.animalName').text(animal.name_german);
         cardTemplate.find('.animalDescription').text(animal.trivia_german);
-        // Update other fields as needed
-        
-        // Append the cloned card to the wrapper
+        cardTemplate.find('.animalGroupNumber').text(animal.group + animal.group_number);
+
         $('#wrapper').append(cardTemplate);
     });
 
-    // Remove the original template card
     $('.animalCardWrapper').first().remove();
 });
